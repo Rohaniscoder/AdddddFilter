5f5de5b7e41482859d68e21379c01c452baf37f4
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2023-10-04 17:03:41 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -166,6 +166,20 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
+async def broadcast_messages_group(chat_id, message):
+    try:
+        kd = await message.copy(chat_id=chat_id)
+        try:
+            await kd.pin()
+        except:
+            pass
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages_group(chat_id, message)
+    except Exception as e:
+        return False, "Error"
+    
 async def search_gagala(text):
     usr_agent = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
